968a55e0a95debde00f029881d8b1a018a0498b4
Koishi <huben2koishi@gmail.com>
2019-02-25 17:59:36 +0800

    UPDATE

--- 19.插入数据.sql
+++ 19.插入数据.sql
@@ -1,11 +1,16 @@
 # INSERT 插入完整的行
 INSERT INTO customers
     VALUE (NULL, 'Pep E. LaPew', '100 Main Street', 'Los Angeles', 'CA', '90046', 'USA', NULL, NULL);
-INSERT INTO customers(cust_id,CUST_NAME, CUST_ADDRESS, CUST_CITY, CUST_STATE, CUST_ZIP, CUST_COUNTRY, CUST_CONTACT, CUST_EMAIL)
+INSERT INTO customers(cust_id, cust_name, cust_address, cust_city, cust_state, cust_zip, cust_country, cust_contact, cust_email)
     VALUE (10006,'Koishi', 'DLD', 'Old Hell', 'HB', '514', 'CN', NULL, NULL);
 # 一般不要使用没有明确给出列的列表的 INSERT 语句
 
 # 插入多个行
-INSERT INTO customers(CUST_NAME, CUST_ADDRESS, CUST_CITY, CUST_STATE, CUST_ZIP, CUST_COUNTRY)
+INSERT INTO customers(cust_name, cust_address, cust_city, cust_state, cust_zip, cust_country)
     VALUE ('Pep E. LaPew', '100 Main Street', 'Los Angeles', 'CA', '90046', 'USA'),
     ('M. Martian', '42 Galaxy Way','New York', 'NY', '11213', 'USA');
+
+# 插入检索出的数据
+INSERT INTO customers(cust_id, cust_name, cust_address, cust_city, cust_state, cust_zip, cust_country, cust_contact, cust_email)
+SELECT cust_id, cust_name, cust_address, cust_city, cust_state, cust_zip, cust_country, cust_contact, cust_email
+FROM customers;
